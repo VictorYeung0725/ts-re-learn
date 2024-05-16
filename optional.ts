b557b2eb1a7d8cd67c8e806eb3042df20b10bc4a
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-05-16 22:55:34 +0800

    add optional in typescript

---
+++ optional.ts
@@ -0,0 +1,33 @@
+//NOTE Optional parameter
+function printIngredient(quantity: string, ingredient: string, extra?: string) {
+  console.log(`${quantity} ${ingredient} ${extra ? `${extra}` : ''}`);
+}
+printIngredient('IC', 'four');
+printIngredient('IC', 'four', 'something mroe');
+
+//NOTE Optional fields
+interface User {
+  id: string;
+  info?: { email?: string };
+}
+
+function getEmail(user: User): string {
+  if (user.info) {
+    return user.info.email;
+  }
+  return '';
+}
+
+//NOTE this one improtant to learn
+function getEmailEasy(user: User): string {
+  return user?.info?.email ?? '';
+}
+
+function addWithCallback(x: number, y: number, callback?: () => void) {
+  console.log(x, y);
+
+  //NOTE what happen if callback is optional?
+  // callback();
+  // if (callback) {}
+  callback?.();
+}
