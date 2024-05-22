41069ccc4fe2677e22247cd5f35968b5ac39e355
VictorYeung0725 <96713542+VictorYeung0725@users.noreply.github.com>
2024-05-22 22:16:36 +0800

    add and review function overload and enums

--- enums.ts
+++ enums.ts
@@ -17,14 +17,26 @@ const englishLoadingState = {
 
 const isLoading = (state: loadingState) => state === loadingState.loading;
 //here complaint, and check the type must between above three type
-console.log(isLoading('dogs'));
+// console.log(isLoading('dogs'));
 
 //What is literal type
 
-function rollDice(dict: number): number {
+function rollDice(dict: 1 | 2 | 3): number {
   let pip = 0;
   for (let i = 0; i < dict; i++) {
     pip += Math.floor(Math.random() * 5) + 1;
   }
   return pip;
 }
+//4 is not allow here if we define the dict number literal type
+// console.log(rollDice(4));
+console.log(rollDice(3));
+
+//here we create function overload
+function sendEveng(name: 'addCart', data: { productId: number }): void;
+function sendEveng(name: 'checkOut', data: { cartCount: number }): void;
+function sendEveng(name: string, data: unknown): void {
+  console.log(`${name}: ${JSON.stringify(data)}`);
+}
+
+sendEvent('checkOut', { productId: 12323 });
